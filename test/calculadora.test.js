2252b93f07590a5af32823e19ae6a68a2c537009
MunGzar <giraldojose814@gmail.com>
2025-12-10 22:43:05 -0500

    modificacion al test

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,15 +1,14 @@
-import { expect } from 'chai';
-import { sumar, restar } from '../src/calculadora.js';
-import { describe, beforeEach, it } from 'mocha';
+const { expect } = require("chai");
+const { sumar, restar } = require("../calculadora");
 
-describe('Calculadora',() => {
+describe("Calculadora", () => {
 
-    it('Funcion sumar 2 + 2 ', () => {
-        expect(sumar(2, 2)).to.equal(4);
-    });
+  it("Funcion sumar 2 + 2", () => {
+    expect(sumar(2, 2)).to.equal(4);
+  });
 
-    it('Funcion restar 4 - 2 ', () => {
-        expect(restar(4, 2)).to.equal(2);
-    });
+  it("Funcion restar 4 - 2", () => {
+    expect(restar(4, 2)).to.equal(2);
+  });
 
-})
+});
